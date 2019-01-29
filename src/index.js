c33a9ccf5c368f85c0f002f73d90ad8a28c21bdc
Jeff Kielman <info@jeffkielman.com>
2019-01-29 18:17:49 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+console.log('Receptor - a11y chrome extension');
+
+
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+// import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './style.scss';
+
+const App = () => (
+  <div>
+   test
+  </div>
+)
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('index')
+)
+
