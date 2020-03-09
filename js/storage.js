fd62c68fbaf46088eb11bdc3b4b975344174fa8d
Jeff Kielman <info@jeffkielman.com>
2020-03-09 00:39:33 -0400

    CSS Update

--- js/storage.js
+++ js/storage.js
@@ -1,24 +1,15 @@
 'use strict';
 
 chrome.storage.local.get(['toggle-display-localstorage'], function(returned) {
-
     if (returned['toggle-display-localstorage'] === 'true') {
-
         const headNew = document.getElementsByTagName('head')[0],
-            src = document.createElement('script');
+        src = document.createElement('script');
         src.src = chrome.extension.getURL('js/inject.js');
         headNew.appendChild(src);
-
         document.querySelector('body').classList.toggle('a11y');
-
     } else {
-
-
-            document.querySelector('body').classList.remove('a11y');
-
-
+    	document.querySelector('body').classList.remove('a11y');
     }
-
 });
 
 
